4ab36da62f4e6e4d807841b31c069534ffca7470
林园园 <linyuanyuan@bluepha.com>
2022-01-09 17:40:07 +0800

    feat: 添加项目

---
+++ core/cli/lib/index.js
@@ -0,0 +1,13 @@
+'use strict';
+/**
+ * 命令执行流程
+ * 准备阶段：检查版本号、检查node版本、检查root自动、检查用户主目录、检查入参、检查变量环境、检查是否为最新版本、提示更新
+ * 命令注册：注册init命令、注册publish命令、注册clean命令、支持debug
+ * 命令执行
+*/
+module.exports = core;
+
+function core() {
+    // TODO
+    console.log('exec core');
+}
