c7cfe7f5fd34cf13c718a2ab466ceb3065e03f7b
linxibei <herryleehom@126.com>
2022-01-11 14:27:30 +0800

    feat: 添加pkg.version和process.version

--- core/cli/bin/index.js
+++ core/cli/bin/index.js
@@ -1,1 +1,7 @@
 #! /usr/bin/env node
+const importLocal = require('import-local');
+if (importLocal(__filename)) {
+	require('npmlog').info('cli', '正在使用lyy-cli-dev本地版本');
+} else {
+	require('../lib')(process.argv.slice(2));
+}
--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -5,9 +5,39 @@
  * 命令注册：注册init命令、注册publish命令、注册clean命令、支持debug
  * 命令执行
 */
+/**
+ * require可以加载哪些文件
+ * 1. 加载.js：必须module.exports/exports
+ * 2. 加载.json: JSON.parse
+ * 3. 加载.node: process.dlopen (C++ AddOn)
+ * 4. 加载 非上述文件 any，会.js引擎进行解析，如果有js代码就正常解析，否则报错
+*/
+const pkg = require('../package.json');
+const log = require('@lyy-cli-dev/log');
+const colors = require('colors/safe');
+const constant = require('./const');
+const semver = require('semver');
 module.exports = core;
 
 function core() {
-    // TODO
-    console.log('exec core');
+  // TODO
+  try {
+    checkPkgVersion();
+    checkNodeVersion();
+  } catch(e) {
+    log.error(e.message);
+  }
+}
+function checkPkgVersion() {
+  log.notice('cli', pkg.version);
+}
+
+function checkNodeVersion() {
+  // 获取当前node版本号
+  const curVersion = process.version;
+  // 比对最低版本号
+  const lowestVersion = constant.LOWEST_NODE_VERSION;
+  if (!semver.gte(curVersion, lowestVersion)) {
+    throw new Error(colors.red(`lyy-cli-dev 需要安装v-${lowestVersion} 以上版本的node.js`));
+  }
 }
